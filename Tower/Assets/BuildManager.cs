6f547fee051305f4a34dd4cbb6f5acbc2b0345a1
Joaoga08 <wandreyschelbauerjoaogabriel@gmail.com>
2024-10-23 13:41:55 -0300

    tower

--- Tower/Assets/BuildManager.cs
+++ Tower/Assets/BuildManager.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class BuildManager : MonoBehaviour
 {
+<<<<<<< Updated upstream
     // Start is called before the first frame update
     void Start()
+=======
+    public static BuildManager instance;
+
+    [Header("References")]
+    
+    [SerializeField] private Tower[] towers;
+
+    private int SelectedTower = 0;
+
+    private void Awake()
+>>>>>>> Stashed changes
     {
         
     }
 
+<<<<<<< Updated upstream
     // Update is called once per frame
     void Update()
     {
         
+=======
+    public Tower GetSelectedTower()
+    {
+        return towers[SelectedTower];
+>>>>>>> Stashed changes
     }
 }
